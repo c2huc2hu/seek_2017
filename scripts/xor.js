5a8f916699a26a795753e18dd799782c53e66b7a
c2huc2hu <c2huc2hu@gmail.com>
2017-11-10 00:46:02 -0500

    changed theme for homepage, adjusted some layout things

--- scripts/xor.js
+++ scripts/xor.js
@@ -1,7 +1,13 @@
 // email address is xor encoded with the key "seek"
+// can generate the message with the python script
+// import itertools
+// s = "string_to_encrypt"
+// print(list(map(lambda x: x[0] ^ x[1], zip(map(ord, s), itertools.cycle(map(ord, 'seek'))))))
+
+
 function xor(key, msg) {
   r=[];
   for(var i=0;i<msg.length;i++)
-    r.push(key.charCodeAt(i % key.length) ^ msg[i]);     
+    r.push(key.charCodeAt(i % key.length) ^ msg[i]);
   return String.fromCharCode.apply(0, r)
 }
